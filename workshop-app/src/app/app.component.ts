7076261f9c83fb5741e93705c2daee8c33ad4f57
GustavoHendrigo <93684708+GustavoHendrigo@users.noreply.github.com>
2025-11-11 19:08:02 -0300

    Fix uppercase pipe availability in app component

--- workshop-app/src/app/app.component.ts
+++ workshop-app/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
-import { NgIf } from '@angular/common';
+import { NgIf, UpperCasePipe } from '@angular/common';
 import { AuthService } from './core/services/auth.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, NgIf],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, NgIf, UpperCasePipe],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
